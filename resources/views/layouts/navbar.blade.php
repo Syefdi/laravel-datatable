fadc7aa0eb0c24800635966fe3d7cfe99eb4f96a
irfan arimiwandi <irfanarimiwandi@gmail.com>
2024-11-10 20:57:59 +0700

    templateting layouts

---
+++ resources/views/layouts/navbar.blade.php
@@ -0,0 +1,11 @@
+<nav class="mt-2"> <!--begin::Sidebar Menu-->
+    <ul class="nav sidebar-menu flex-column" data-lte-toggle="treeview" role="menu" data-accordion="false">
+        <li class="nav-item"> <a href="#" class="nav-link"> <i class="nav-icon bi bi-box-seam-fill"></i>
+                <p>
+                    Widgets
+                    <i class="nav-arrow bi bi-chevron-right"></i>
+                </p>
+            </a>
+        </li>
+    </ul> <!--end::Sidebar Menu-->
+</nav>
