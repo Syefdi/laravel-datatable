bb926a9ef1b5873befe3067d3a1383f68997e878
Zidan <zidanakbar1904@gmail.com>
2024-11-16 21:53:42 +0700

    up table style

--- resources/views/content/index.blade.php
+++ resources/views/content/index.blade.php
@@ -2,10 +2,11 @@
 @section('content')
     <div class="content">
         <div class="row">
-            <h1>Product List</h1>
-            <a href="{{ route('products.create') }}">Create New Product</a>
-            <table border="1">
-                <thead>
+        <h1 class="my-4">Product List</h1>
+        <a href="{{ route('create') }}" class="btn btn-primary mb-3">Create New Product</a>
+        <div class="table-responsive">
+            <table class="table table-bordered table-hover">
+                <thead class="table-dark">
                     <tr>
                         <th>Name</th>
                         <th>Description</th>
@@ -22,11 +23,11 @@
                             <td>{{ $product->price }}</td>
                             <td>{{ $product->quantity }}</td>
                             <td>
-                                <a href="{{ route('products.edit', $product->id) }}">Edit</a>
-                                <form action="{{ route('products.destroy', $product->id) }}" method="POST" style="display:inline;">
+                                <a href="{{ route('products.edit', $product->id) }}" class="btn btn-warning btn-sm">Edit</a>
+                                <form action="{{ route('products.destroy', $product->id) }}" method="POST" class="d-inline">
                                     @csrf
                                     @method('DELETE')
-                                    <button type="submit">Delete</button>
+                                    <button type="submit" class="btn btn-danger btn-sm">Delete</button>
                                 </form>
                             </td>
                         </tr>
@@ -34,6 +35,8 @@
                 </tbody>
             </table>
         </div>
+    </div>
+
         <div class="row">
             {{-- CONTENT ARTIKEL --}}
             <div class="col-12 mt-1">
--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,35 +1,39 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Product List</h1>
-    <a href="{{ route('create') }}">Create New Product</a>
-    <table border="1">
-        <thead>
-            <tr>
-                <th>Name</th>
-                <th>Description</th>
-                <th>Price</th>
-                <th>Quantity</th>
-                <th>Actions</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($products as $product)
-                <tr>
-                    <td>{{ $product->name }}</td>
-                    <td>{{ $product->description }}</td>
-                    <td>{{ $product->price }}</td>
-                    <td>{{ $product->quantity }}</td>
-                    <td>
-                        <a href="{{ route('products.edit', $product->id) }}">Edit</a>
-                        <form action="{{ route('products.destroy', $product->id) }}" method="POST" style="display:inline;">
-                            @csrf
-                            @method('DELETE')
-                            <button type="submit">Delete</button>
-                        </form>
-                    </td>
-                </tr>
-            @endforeach
-        </tbody>
-    </table>
+<div class="container">
+        <h1 class="my-4">Product List</h1>
+        <a href="{{ route('create') }}" class="btn btn-primary mb-3">Create New Product</a>
+        <div class="table-responsive">
+            <table class="table table-bordered table-hover">
+                <thead class="table-dark">
+                    <tr>
+                        <th>Name</th>
+                        <th>Description</th>
+                        <th>Price</th>
+                        <th>Quantity</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($products as $product)
+                        <tr>
+                            <td>{{ $product->name }}</td>
+                            <td>{{ $product->description }}</td>
+                            <td>{{ $product->price }}</td>
+                            <td>{{ $product->quantity }}</td>
+                            <td>
+                                <a href="{{ route('products.edit', $product->id) }}" class="btn btn-warning btn-sm">Edit</a>
+                                <form action="{{ route('products.destroy', $product->id) }}" method="POST" class="d-inline">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit" class="btn btn-danger btn-sm">Delete</button>
+                                </form>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
 @endsection
