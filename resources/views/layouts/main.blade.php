a4781d662249c1a96549d348a940e6fc5231c1b5
irfan arimiwandi <irfanarimiwandi@gmail.com>
2024-11-16 18:32:13 +0700

    fix

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -4,7 +4,7 @@
 
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-    <title>AdminLTE | Dashboard v2</title><!--begin::Primary Meta Tags-->
+    <title>Project Bersama</title><!--begin::Primary Meta Tags-->
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="title" content="AdminLTE | Dashboard v2">
     <meta name="author" content="ColorlibHQ">
