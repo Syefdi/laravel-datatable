fe1e252be03a1ce883d8d079aabc7a488d17f09f
Zidan <zidanakbar1904@gmail.com>
2024-12-12 19:34:59 +0700

    ad form login

--- resources/views/zidan/blog/header.blade.php
+++ resources/views/zidan/blog/header.blade.php
@@ -4,4 +4,5 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
     <title>Document</title>
     <link rel="stylesheet" href="{{ asset('zidan/zidan.css')}}">
+    
 </head>
